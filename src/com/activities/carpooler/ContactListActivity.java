de6b31aed88fee1bcb780012991f6dffe767fc57
lacomblezl <loic.lacomblez@student.uclouvain.be>
2014-01-31 22:37:56 +0100

    Reorganisation activites et xml

--- src/com/activities/carpooler/ContactListActivity.java
+++ src/com/activities/carpooler/ContactListActivity.java
@@ -1,17 +1,25 @@
 package com.activities.carpooler;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class ContactListActivity extends Activity
 {
-
+	// Liste de strings "nom prenom" des Contacts
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_contact_list);
+		setContentView(R.layout.activity_simple_list);
+		
+		/* Mise a jour du titre */
+		TextView title = (TextView) findViewById(R.id.title);
+		title.setText(getText(R.string.contacts));
+		
+		//TODO remplissage de la liste et listener dessus !
 	}
 
 	@Override
--- src/com/activities/carpooler/RoadListActivity.java
+++ src/com/activities/carpooler/RoadListActivity.java
@@ -3,6 +3,7 @@ package com.activities.carpooler;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class RoadListActivity extends Activity
 {
@@ -11,7 +12,13 @@ public class RoadListActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_road_list);
+		setContentView(R.layout.activity_simple_list);
+		
+		/* Mise a jour du titre */
+		TextView title = (TextView) findViewById(R.id.title);
+		title.setText(getText(R.string.roads));
+		
+		//TODO remplissage de la liste et listener dessus !
 	}
 
 	@Override
